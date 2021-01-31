599448ca355adcdd68a7b35bc428d3fc498d4a31
arturvoloshyn <arthurvoloshyn@gmail.com>
2021-01-31 19:39:12 +0200

    Update MoviesDialog cpmponent

--- application/src/components/MoviesDialog/MoviesDialog.jsx
+++ application/src/components/MoviesDialog/MoviesDialog.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -10,42 +11,50 @@ import BlockIcon from '@material-ui/icons/Block';
 
 import withHocs from './MoviesDialogHoc';
 
-class MoviesDialog extends React.Component {
-  handleDelete = () => {
-    const { id, handleClose, deleteMovie } = this.props;
+const MoviesDialog = ({ id, handleClose, deleteMovie, open }) => {
+  const handleDelete = () => {
     deleteMovie(id);
     handleClose();
   };
 
-  render() {
-    const { open, handleClose } = this.props;
+  return (
+    <Dialog
+      aria-describedby="alert-dialog-description"
+      aria-labelledby="alert-dialog-title"
+      onClose={handleClose}
+      open={open}
+    >
+      <DialogTitle id="alert-dialog-title">
+        Are you sire that you want to delete element?
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          If you click &lsquo;Confirm&rsquo; this element will be removed from data base.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button color="primary" onClick={handleClose}>
+          <BlockIcon /> Cancel
+        </Button>
+        <Button autoFocus color="primary" onClick={handleDelete}>
+          <DeleteForeverIcon /> Confirm
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
-    return (
-      <Dialog
-        aria-describedby="alert-dialog-description"
-        aria-labelledby="alert-dialog-title"
-        onClose={handleClose}
-        open={open}
-      >
-        <DialogTitle id="alert-dialog-title">
-          Are you sire that you want to delete element?
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            If you click 'Confirm' this element will be removed from data base.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button color="primary" onClick={handleClose}>
-            <BlockIcon /> Cancel
-          </Button>
-          <Button autoFocus color="primary" onClick={this.handleDelete}>
-            <DeleteForeverIcon /> Confirm
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
-}
+MoviesDialog.propTypes = {
+  id: PropTypes.string.isRequired,
+  handleClose: PropTypes.func,
+  deleteMovie: PropTypes.func,
+  open: PropTypes.bool,
+};
+
+MoviesDialog.defaultProps = {
+  handleClose: () => {},
+  deleteMovie: () => {},
+  open: false,
+};
 
 export default withHocs(MoviesDialog);
